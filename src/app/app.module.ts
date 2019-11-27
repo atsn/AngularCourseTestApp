e64c9f5b2f653dcb241190813ca062b57d1f24b9
Anders Thomhav Stubberup (ADT) <ADT@kmd.dk>
2019-11-27 14:47:18 +0100

    Added lefledModule to add module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { LeafletModule } from './leaflet';
 
 @NgModule({
   declarations: [AppComponent, SidebarComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, LeafletModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
